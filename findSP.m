995ab90415d519873cc90b72face6b98c34b3025
notalol1con <zbl183905063@gmail.com>
2023-05-12 05:23:46 +1000

    added changed to BFGS

--- findSP.m
+++ findSP.m
@@ -7,8 +7,8 @@ k = 0;
 uk = [100 100 100];
 while (k==0 || norm(uprev - uk) > tolerance3) 
     uprev = uk;
-    ak = k;
-    fmin = inf;
+    ak = 5*(k+1);
+    fmin = 100;
     umin = [-1 -1 -1];
     for j = 1:numTries
         u0 = rand(1,3);
--- testing.m
+++ testing.m
@@ -1,6 +1,6 @@
 alpha = 1;
-u = [1,0,0];
+u = [-1,0,0];
 Y = [0 0];
 
 % runBuiltin(u,Y)
-findSP('steepestDescentMethod','l2','gradl2', Y,0.01,0.01,0.01,2,10)
+findSP('steepestDescentMethod','l2','gradl2', Y,0.001,0.001,0.1,2,10)
